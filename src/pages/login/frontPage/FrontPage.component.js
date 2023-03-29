4b67afed0f87a2711fd421fd0f02b0f2923ac7a8
Max Klein <mcklein1992@gmail.com>
2023-03-29 19:02:34 +0100

    random background image

--- src/pages/login/frontPage/FrontPage.component.js
+++ src/pages/login/frontPage/FrontPage.component.js
@@ -4,11 +4,42 @@ import styles from "./FrontPage.module.scss";
 import Header from "../../../components/header/Header.component";
 import Button from "../../../elements/button";
 
+import PositanoImage from "./positano.png";
+import FlorianopolisImage from "./florianopolis.png";
+import FlorianopolisImage2 from "./florianopolis2.png";
+import FlorianopolisImage3 from "./florianopolis3.png";
+import RioDeJaneiroImage from "./riodejaneiro.png";
+import SpainImage from "./spain.png";
+import ItalyImage from "./italy.png";
+import ItalyImage2 from "./italy2.png";
+
+const randomSelectBackgroundImage = () => {
+  const backgroundImages = [
+    PositanoImage,
+    FlorianopolisImage,
+    FlorianopolisImage2,
+    FlorianopolisImage3,
+    RioDeJaneiroImage,
+    SpainImage,
+    ItalyImage,
+    ItalyImage2,
+  ];
+
+  const randomNumber = Math.floor(Math.random() * backgroundImages.length);
+
+  return backgroundImages[randomNumber];
+};
+
 const FrontPage = () => {
   return (
     <div className={styles.component}>
       <div className={styles.backgroundImageContainer}>
-        <div className={styles.backgroundImage} />
+        <div
+          className={styles.backgroundImage}
+          style={{
+            backgroundImage: "url(" + randomSelectBackgroundImage() + ")",
+          }}
+        />
       </div>
       <div className={styles.headerContainer}>
         <Header transparent={true} />
